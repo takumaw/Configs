9fdcec8b43dffc3b92debe6579388f85d703235a
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2024-03-18 23:25:31 +0900

    Fix installer.

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Configs initializer
 #
 # See also: https://dotfiles.github.io/tutorials/
 #
 
+# Save the repository path
+REPO_DIR=${PWD#"$HOME"/}
+
 # Move to the home directory
-cd $HOME
+cd ${HOME}
 
 # Create symlinks
-ln -s Configs/Editor/.screenrc
-ln -s Configs/Editor/.vimrc
+ln -s ${REPO_DIR}/Editor/.screenrc
+ln -s ${REPO_DIR}/Editor/.vimrc
 
-ln -s Configs/Git/.gitconfig
-ln -s Configs/Git/.gitignore_global
+ln -s ${REPO_DIR}/Git/.gitconfig
+ln -s ${REPO_DIR}/Git/.gitignore_global
 
-ln -s Configs/Shell/.zshrc
+ln -s ${REPO_DIR}/Shell/.zshrc
 
-ln -s Configs/SSH .ssh
+ln -s ${REPO_DIR}/SSH .ssh
 chmod 600 .ssh/*
